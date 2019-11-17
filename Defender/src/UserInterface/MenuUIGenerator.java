709fd787fb525f4d73dd34fdd08b5ddb1375ca16
Hanzallah <hanzallahazimburney@live.com>
2019-11-17 15:38:53 +0300

    Test Commit

--- Defender/src/UserInterface/MenuUIGenerator.java
+++ Defender/src/UserInterface/MenuUIGenerator.java
@@ -14,7 +14,7 @@ public class MenuUIGenerator extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        
+
     }
 
     public static void main(String[] args){
