8bbcd8ef3259ada724f4a95b378157771f4ac398
Hanzallah <hanzallahazimburney@live.com>
2019-11-18 10:30:52 +0300

    Game + State

--- Defender/src/Game/GameState.java
+++ Defender/src/Game/GameState.java
@@ -1,16 +1,6 @@
 package Game;
 
-import GameEngine.GameEngine;
-import UserInterface.MyApplication;
 
 public class GameState {
-    private GameEngine ge;
-    private MyApplication app;
-
-    public GameState(){
-        ge = new GameEngine();
-
-    }
-
-
+    // TODO
 }
