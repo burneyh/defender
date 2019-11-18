92b6796b46f0bcc5f7a4cac889bdee5b22fb90cd
Hanzallah <hanzallahazimburney@live.com>
2019-11-18 22:05:25 +0300

    Humans Initialized

--- Defender/src/GameObjects/Human.java
+++ Defender/src/GameObjects/Human.java
@@ -1,10 +1,10 @@
 package GameObjects;
 
+import UserInterface.MyApplication;
+
 public class Human extends GameObject{
     public Human(){
-        super();
-        this.setX(5);
-        this.setY(5);
+        super((int)(Math.random() * (MyApplication.WIDTH -20)), 485);
         loadImage("Icons/003-workers.png");
         getImageDimensions();
     }
