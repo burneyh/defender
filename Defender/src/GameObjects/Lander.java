f6134e6982dbfe1d4daed3aeaf8bef8ef565a11f
AbdulHamidDabboussi <abdulhamid.dabboussi@gmail.com>
2019-11-18 11:37:11 +0300

    Added constructor to Lander Class.

--- Defender/src/GameObjects/Lander.java
+++ Defender/src/GameObjects/Lander.java
@@ -1,6 +1,16 @@
 package GameObjects;
 
+import java.util.Random;
+
 public class Lander extends Alien {
+
+    public Lander() {
+        super(((int)(Math.random() * 500)), (int)(Math.random()) * 400);
+        points = 150;
+        loadImage("res/Icons/002-lander.png");
+        getImageDimensions();
+    }
+
     public Lander(int x, int y) {
         super(x, y);
         points = 150;
