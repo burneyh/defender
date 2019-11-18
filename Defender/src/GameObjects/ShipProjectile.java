571bb22820220165b85ad1ea5dc8cd092394acff
AbdulHamidDabboussi <abdulhamid.dabboussi@gmail.com>
2019-11-18 19:07:23 +0300

    Edits.

--- Defender/src/GameObjects/ShipProjectile.java
+++ Defender/src/GameObjects/ShipProjectile.java
@@ -4,7 +4,9 @@ public class ShipProjectile extends Projectile{
 
     public ShipProjectile(Coordinate coordinates, moveDirection direction){
         super(coordinates.getX(), coordinates.getY());
-        loadImage("res/Icons/004-rocket-1.png");
+
+        loadImage("Icons/004-rocket-1.png");
+        this.getImageDimensions();
         this.setCoordinates(coordinates);
         this.setDirection(direction);
         this.move(direction);
