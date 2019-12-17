84dc70893ee37100bead1fb5c338830432ff47b2
AbdulHamidDabboussi <abdulhamid.dabboussi@gmail.com>
2019-12-17 20:50:13 +0300

    Edited PowerUp class.

--- Defender/src/GameObjects/PowerUp.java
+++ Defender/src/GameObjects/PowerUp.java
@@ -1,4 +1,10 @@
 package GameObjects;
 
-public class PowerUp {
+public class PowerUp extends GameObject {
+
+    public static enum Type {REFILL_HEALTH, SHIELD, TRIPLE_SHOP, EMPOWERED_SHOT, EXPLOSIVE_SHOT, FROST}
+
+    public PowerUp(int x, int y) {
+        super(x, y);
+    }
 }
