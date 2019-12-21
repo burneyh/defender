039764addcd5719c5b713039a7b87d7ca1552b13
3ndr1 <37934972+3ndr1@users.noreply.github.com>
2019-12-21 16:08:55 +0300

    level change movement fix

--- Defender/src/Controllers/GameEngine.java
+++ Defender/src/Controllers/GameEngine.java
@@ -327,6 +327,11 @@ public class GameEngine implements EventHandler<KeyEvent> {
 
     private void nextLevel() {
         motherShip.resetPos();
+        isFire =false;
+        isUp = false;
+        isDown = false;
+        isRight = false;
+        isLeft = false;
         score = 0;
         levelManager.incrementLevel();
 
